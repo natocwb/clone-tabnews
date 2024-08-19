cae1604868fbacc503f3008ed865da38abd25c82
natocwb <53237929+natocwb@users.noreply.github.com>
2024-08-19 20:59:16 +0000

    finish endpont /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,33 @@
 import database from "infra/database.js";
 async function status(request, response) {
-  const result = await database.query("SELECT 1 as conta1;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Alunos do curso.dev são pessoas " });
+  const updatedAt = new Date().toISOString();
+  const dataBaseVersionResult = await database.query("SHOW server_version;");
+  const dataBaseVersionValue = dataBaseVersionResult.rows[0].server_version;
+
+  const dataBaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const dataBaseMaxConnectionsValue =
+    dataBaseMaxConnectionsResult.rows[0].max_connections;
+
+  const dataBaseName = process.env.POSTGRES_DB;
+  const dataBaseOpenConnectionsResult = await database.query({
+    text: "select count(*)::int used from pg_stat_activity where datname =$1;",
+    values: [dataBaseName],
+  });
+  const dataBaseOpenConnectionsValue =
+    dataBaseOpenConnectionsResult.rows[0].used;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dataBaseVersionValue,
+        max_connections: parseInt(dataBaseMaxConnectionsValue),
+        opened_connections: dataBaseOpenConnectionsValue,
+      },
+    },
+  });
 }
+
 export default status;
