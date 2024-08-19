2c2acc1e416b45d6115ffc82bf643cb92e967658
natocwb <53237929+natocwb@users.noreply.github.com>
2024-08-19 14:06:53 +0000

    add  and

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
-  response.status(200).json({ chave: "Alinos do curso.dev são pessoas " });
+import database from "../../../../infra/database.js";
+async function status(request, response) {
+  const result = await database.query("SELECT 1 as conta1;");
+  console.log(result.rows);
+  response.status(200).json({ chave: "Alunos do curso.dev são pessoas " });
 }
 export default status;
