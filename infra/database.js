c112ca1037592bfdd70d79052587a3b3d838c09a
natocwb <53237929+natocwb@users.noreply.github.com>
2024-09-16 03:02:26 +0000

    add api v1 migrations endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,5 +1,18 @@
 import { Client } from "pg";
 async function query(queryObject) {
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
+}
+
+async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_POST,
@@ -9,17 +22,11 @@ async function query(queryObject) {
     ssl: getSSLValues(),
   });
   await client.connect();
-  try {
-    const result = await client.query(queryObject);
-    return result;
-  } catch (error) {
-    console.error(error);
-  } finally {
-    await client.end();
-  }
+  return client;
 }
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
 
 function getSSLValues() {
@@ -29,5 +36,5 @@ function getSSLValues() {
     };
   }
 
-  return process.env.NODE_ENV === "development" ? false : true;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,23 @@
+import database from "infra/database";
+beforeAll(clearDatabase);
+async function clearDatabase() {
+  await database.query("drop schema public cascade;create schema public;");
+}
+
+test("GET to api/v1/migrations should be 200 ", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response.status).toBe(201);
+  const responseBody = await response.json();
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+  const responseBody2 = await response2.json();
+  expect(Array.isArray(responseBody2)).toBe(true);
+  expect(responseBody2.length).toBe(0);
+});
