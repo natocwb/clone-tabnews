edd5804cb56aadf8f33837870e1ddbf6a6c8c114
natocwb <53237929+natocwb@users.noreply.github.com>
2024-09-17 02:10:41 +0000

    makes npm test more robust

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,6 @@ const createJestConfig = nextJest({
 });
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
+  testTimeout: 60000,
 });
 module.exports = jestConfig;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,9 @@
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServers();
+});
+
 test("GET to api/v1/status should be 200 ", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
